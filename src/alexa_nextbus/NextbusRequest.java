5dc9d0d7f12a6664d09d229eda0ef232e19e840d
Bhargav Mehta <bhargav.mehta01@gmail.com>
2017-10-02 15:26:08 -0400

    changes

--- src/alexa_nextbus/NextbusRequest.java
+++ src/alexa_nextbus/NextbusRequest.java
@@ -2,6 +2,8 @@ package alexa_nextbus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -9,44 +11,49 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class NextbusRequest {
-	
+
 	public static String predictions(String busRoute, String busStop) {
-		//this is a test URL which works for everytime
-		String testUrl = "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=rutgers&r=wknd1&s=livingston";
-		String requestUrl = "";
+
+		//this is a test URL which works for every time
+		//String testUrl = "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=rutgers&r=wknd1&s=livingston";
+		String baseUrl = "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=rutgers";
+		StringBuilder requestUrl = new StringBuilder(baseUrl);
 		String route = "";
 		String stop = "";
-		//String lat = "";
-		String latlongt = "";
-		String latlongreqUrl = "";
-		
+
+		System.out.println(busRoute + " " + busStop);
 		List<String> time = new ArrayList<String>(); 
+
+		Map<String, String> routes = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+		Map<String, String> stops = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
 		
-		if(busRoute.equalsIgnoreCase("campus connect")) {
-			route = "connect";
-			//lat = "40.7437891,";
-		}
-			
+		routes.put("campus connect", "connect");
 		
-		if(busStop.equalsIgnoreCase("washington park")) {
-			stop = "washpark";
-			latlongt = "40.7437891,-74.170569";
-		}
-			
+		stops.put("washington park", "washpark");
 		
-		latlongreqUrl = "http://restbus.info/api/locations/".concat(latlongt).concat("/predictions");
-		requestUrl = ("http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=rutgers-newark").concat("&r=").concat(route).concat("&s=").concat(stop);
-		
+		route = routes.get(busRoute);
+		stop = stops.get(busStop);
+		System.out.println(route + " " + stop);
+//		if(busRoute.equalsIgnoreCase("campus connect")) {
+//			route = "connect";
+//		}
+//
+//		if(busStop.equalsIgnoreCase("washington park")) {
+//			stop = "washpark";
+//		}
+
+		requestUrl.append("&r=").append(route).append("&s=").append(stop);
+
 		if(busRoute.equalsIgnoreCase("test") && busStop.equals("test")) {
 			route = "test";
 			stop = "test";
-			requestUrl = testUrl;
+			//requestUrl = testUrl;
 		}
-			
-		GetMethod method = new GetMethod(latlongreqUrl);
-		
+
+		GetMethod method = new GetMethod(requestUrl.toString());
+
 		method.addRequestHeader("Accept", "application/json");
-		
+
 		String response = "";
 		HttpClient client = new HttpClient();
 		try {
@@ -54,29 +61,28 @@ public class NextbusRequest {
 
 			if(statusCode == 200){
 				response = method.getResponseBodyAsString();
-				
+
 				JSONObject res = new JSONObject(response);
 				JSONArray pred = new JSONArray();
-				
+
 				JSONObject data = res.getJSONObject("predictions");
 
 				if(data.has("direction"))
 					pred = data.getJSONObject("direction").getJSONArray("prediction"); 
 				else
 					return "No predictions found. Please try again later.";
-				
+
 				pred.forEach(item -> {
 					JSONObject minute = (JSONObject)item;
 					time.add(minute.getString("minutes"));
 				});
-				
+
 				System.out.println(time.toString());
-				
-				String result = "The expected ETA for the shuttle at ".concat(stop).concat(" for ").concat(route).concat(" is ").concat(time.toString());
+
+				String result = "The ETA for ".concat(busRoute).concat(" at ").concat(busStop).concat(" is ").concat(time.toString()).concat(" minutes");
 				System.out.println(result);
 				return result;
 			}
-
 			else{
 				return "Error! Please try again";
 			}
@@ -85,5 +91,5 @@ public class NextbusRequest {
 			return "Error!";
 		}
 	}
-	
+
 }
